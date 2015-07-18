c2855d9f2d8f591326e8e3c885f2982f630f83cb
Ismael Jimenez <ijimenezgarzon@gmail.com>
2015-07-18 16:43:42 -0600

    updated gulp file with the new dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,33 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var cssmin = require('gulp-cssmin');
 var jshint = require('gulp-jshint');
+var sass = require('gulp-ruby-sass');
+
 
 gulp.task('scripts', function() {
     return gulp.src(['bower_components/jquery/dist/jquery.js',
-        'bower_components/bootstrap-material-design/scripts/*.js'])
+        'bower_components/material-design-lite/material.js'])
       .pipe(concat('libs.js'))
       .pipe(rename({suffix: '.min'}))
       .pipe(uglify())
-      .pipe(gulp.dest('build'));
+      .pipe(gulp.dest('dist'));
 });
 
-var sass = require('gulp-ruby-sass');
-
 gulp.task('css', function() {
-    return gulp.src(['bower_components/bootstrap-material-design/dist/css/*.css'])
+    return gulp.src(['bower_components/material-design-lite/meterial.css'])
       .pipe(concat('external.css'))
       .pipe(cssmin())
       .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('build'));
+      .pipe(gulp.dest('dist'));
 });
+
 
 gulp.task('watch-libs', function() {
    // Watch .js files
   gulp.watch(['bower_components/jquery/dist/jquery.js',
-        'bower_components/bootstrap-material-design/scripts/*.js'], ['scripts']);
+        'bbower_components/material-design-lite/material.js'], ['scripts']);
    // Watch .scss files
-  gulp.watch('bower_components/bootstrap-material-design/dist/css/*.css', ['css']);
+  gulp.watch('bower_components/material-design-lite/meterial.css', ['css']);
 });
 
 gulp.task('watch', function() {
