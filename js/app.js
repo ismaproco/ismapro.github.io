779196e165eeba7b8716ef4ca620ee7e1e5c0981
Ismael Jimenez <ijimenezgarzon@gmail.com>
2015-07-22 07:39:01 -0600

    Add controller to the page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 'use strict'
+
+var app = angular.module('portfolio',[]);
+
+app.controller('basicInformation',function() {
+    this.bio = {
+        name : 'Ismael Jimenez',
+        role : 'Software Developer',
+        contacts : {
+              mobile: '+(506) 6051 1105',
+              email: 'isma@ismapro.com',
+              github: 'ismaproco',
+              twitter: '@ismapro',
+              location: 'Costa Rica'
+              },
+        welcomeMessage: 'This is my Application Development Portfolio',
+        skills: ['C#','Javascript','jQuery', 'Ajax', 'SharePoint', 'Python' , 'node.js'],
+        biopic: '/images/avatar.jpg'
+    }
+});
