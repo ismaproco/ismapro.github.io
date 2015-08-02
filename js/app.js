e30449400191f5c1a03781faa40f34c6bdb7eed7
Ismael Jimenez <ijimenezgarzon@gmail.com>
2015-08-02 17:33:14 -0600

    improve styling and add more attributes to the bio

--- js/app.js
+++ js/app.js
@@ -5,16 +5,19 @@ var app = angular.module('portfolio',[]);
 app.controller('basicInformation',function() {
     this.bio = {
         name : 'Ismael Jimenez',
-        role : 'Software Developer',
+        role : 'Web Developer',
         contacts : {
               mobile: '+(506) 6051 1105',
               email: 'isma@ismapro.com',
               github: 'ismaproco',
               twitter: '@ismapro',
-              location: 'Costa Rica'
+              location: 'Costa Rica',
+              blog: 'http://ismapro.com'
               },
-        welcomeMessage: 'This is my Application Development Portfolio',
+        welcomeMessage: 'Welcome to my Portfilo',
+        welcomeDescription: 'In this portfolio you will find the my experience as Web Developer, the main projects I hed work on and some other details about me.',
         skills: ['C#','Javascript','jQuery', 'Ajax', 'SharePoint', 'Python' , 'node.js'],
         biopic: '/images/avatar.jpg'
     }
 });
+
